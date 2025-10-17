c08715512df87189ad5d16669a661d4a2ed4fe0f
TurboGarry <realgaurav68@gmail.com>
2025-10-17 22:27:39 +0530

    add project 3

---
+++ emailSender.py
@@ -0,0 +1,11 @@
+import smtplib
+
+to = input("Enter Recipient's Email: \n")
+content = input("Enter content for Email: \n")
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()           #starts communication with server
+    server.starttls()
+    server.login('realgaurav68@gmail.com', 'Gaurav@lu#2344')
+    server.sendmail('')

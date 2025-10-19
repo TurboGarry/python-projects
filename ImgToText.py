8b48c686250c6085bc789a57cb2e5386975e0b46
TurboGarry <realgaurav68@gmail.com>
2025-10-19 23:04:13 +0530

    add project 4

---
+++ ImgToText.py
@@ -0,0 +1,8 @@
+import pytesseract
+import os
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"    # Path to tesseract
+
+def convert():
+    img = Image.open('2.jpg')

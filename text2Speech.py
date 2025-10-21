b89b7a63010a5336c9ab039b5b196ef787e2cd72
TurboGarry <realgaurav68@gmail.com>
2025-10-21 09:18:48 +0530

    fix issues

--- text2Speech.py
+++ text2Speech.py
@@ -1,7 +1,7 @@
 import pyttsx3
 
-# data = input("Enter the text you want to convert to speech: ")
+data = input("Enter the text you want to convert to speech: \n")
 
 engine = pyttsx3.init()
-engine.say("hello world")
+engine.say(data)
 engine.runAndWait()

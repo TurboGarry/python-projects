2ff4f200f1af2d40090b33560974226ffe472dbb
TurboGarry <realgaurav68@gmail.com>
2025-10-23 23:44:25 +0530

    add functionality

--- WebcamApp.py
+++ WebcamApp.py
@@ -1,3 +1,12 @@
 import cv2
 
 imgcapture = cv2.VideoCapture(0)
+result = True
+
+while(result):
+    ret, frame = imgcapture.read()
+    cv2.imwrite("tes.jpg", frame)
+    result = False
+    print("Image Captured...")
+
+imgcapture.release()

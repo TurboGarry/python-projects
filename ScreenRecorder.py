dd4e9b1265eda960892962131c1302105289c68a
TurboGarry <realgaurav68@gmail.com>
2025-10-24 08:56:38 +0530

    add loop

--- ScreenRecorder.py
+++ ScreenRecorder.py
@@ -4,4 +4,19 @@ from PIL import ImageGrab
 
 def ScreenRecorder():
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter("output.avi", fourcc)
+    out = cv2.VideoWriter("output.avi", fourcc, 30.0, (1366, 768))
+
+    while True:
+        img = ImageGrab.grab()
+        img_np = numpy.array(img)
+        frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+        cv2.imshow("Screen Recorder", frame)
+        out.write(frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+    out.release()
+    cv2.destroyAllwindows()
+
+ScreenRecorder()

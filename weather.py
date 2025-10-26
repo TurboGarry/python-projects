0cc06f316ea026dae44b0df67b216bd58d879142
TurboGarry <realgaurav68@gmail.com>
2025-10-26 21:17:57 +0530

    import libraries

--- weather.py
+++ weather.py
@@ -1,1 +1,9 @@
-import 
+import requests
+from bs4 import BeautifulSoup
+
+search = "weather in Delhi"
+url = f"https://www.google.com/search?q={search}"
+
+r = requests.get(url)
+
+s = BeautifulSoup(r.text)
